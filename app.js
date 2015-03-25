a5018d8ef34543a9c079f5edd2387ce4d4402172
William LA PORTE <laport_w@etna-alternance.net>
2015-03-25 23:13:52 +0100

    modif du texte
    modif du texte + modif du test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var hellobot   = require('./hellobot');
 app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
-app.get('/', function (req, res) { res.status(200).send("Vous venez de lancer le standupmeeting!\\n@channel c'est l'heure du StandUp Meeting !\\nPrenez le temps de répondre à ces quelques questions:\\n\\n1 - Sur quoi avez-vous travaillé hier ?\\n2 - Sur quoi allez-vous travailler aujourd'hui ?\\n3 - Y a-t'il quelque chose qui vous bloque ?\\n4 - Avez vous besoin d'aide ?\\n") });
+app.get('/', function (req, res) { res.status(200).send("test") });
  
 // error handler
 app.use(function (err, req, res, next) {
