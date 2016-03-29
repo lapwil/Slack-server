1e2b228199952f6d871d87f11ca86fd8d0b45df4
William LA PORTE <laport_w@etna-alternance.net>
2016-03-29 17:22:49 +0200

    bon

--- hellobot.js
+++ hellobot.js
@@ -1,9 +1,9 @@
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : "Merci, " + userName + " de m'avoir invoqué!\nVous venez de lancer le standupmeeting!\n@channel c'est l'heure du StandUp Meeting !\nPrenez le temps de répondre à ces quelques questions:\n\n1 - Avez vous commencé tous vos projets du run en cours ?\n2 - Sur quoi allez-vous travailler aujourd'hui ?\n3 - Y a-t'il quelque chose qui vous bloque ?\n4 - Avez vous besoin d'aide ?\n5 - Qu'est-ce qui vous a amusé hier? (jeu, vidéo, film, série...)\n"
+    text : "Merci, " + userName + " de m'avoir invoqué!\nVous venez de lancer le Stand Up Meeting!\n@channel c'est l'heure!\nPrenez le temps de répondre à ces quelques questions:\n\n1 - Avez vous commencé tous vos projets du run en cours ?\n2 - Sur quoi avez-vous travaillé hier ?\n3 - Sur quoi allez-vous travailler aujourd'hui ?\n3 - Y a-t'il quelque chose qui vous bloque ?\n4 - Avez vous besoin d'aide ?\n5 - Qu'est-ce qui vous a amusé hier? (jeu, vidéo, film, série...)\n"
   };
- 
+
   // on check le user pour éviter que Slackbot boucle sur lui même
   if (userName !== 'slackbot') {
     return res.status(200).json(botPayload);
